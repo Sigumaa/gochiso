f17668f3d45a023bdfdc2b2c7c5c7944427ae2db
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2024-02-20 23:14:15 +0900

    feat: basic auth

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
 	"time"
@@ -11,7 +12,19 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("please set environment variables")
+	}
+
+	if os.Getenv("BASIC_AUTH_USER") == "" || os.Getenv("BASIC_AUTH_PASS") == "" {
+		log.Println("please set BASIC_AUTH_USER and BASIC_AUTH_PASS")
+		os.Exit(1)
+	}
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -33,11 +46,19 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	// 認証無し
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
 
-	// graceful shutdown
+	// 認証が必要なグループ
+	// 今回は例としてBasic認証を用いる
+	r.Group(func(r chi.Router) {
+		r.Use(middleware.BasicAuth("admin area", map[string]string{os.Getenv("BASIC_AUTH_USER"): os.Getenv("BASIC_AUTH_PASS")}))
+		r.Get("/admin", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("admin page"))
+		})
+	})
 
 	srv := &http.Server{
 		Addr:    ":3333",
@@ -49,8 +70,9 @@ func main() {
 			log.Println("server error", err)
 		}
 	}()
-
 	log.Println("Server is ready to handle requests at :3333")
+
+	// graceful shutdown
 	<-ctx.Done()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -59,6 +81,5 @@ func main() {
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Println("Failed to gracefully shutdown the server", err)
 	}
-
 	log.Println("Server shutdown")
 }
